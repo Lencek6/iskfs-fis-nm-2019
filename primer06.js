f9eccb2c9f678db2a52178734113c49c62e9944c
Lencek6 <lencek113@gmail.com>
2019-02-25 17:42:55 +0000

    dodajanje primera 6

--- primer06.js
+++ primer06.js
@@ -6,12 +6,12 @@ var fs = require("fs"); // knjižnica za delo z datotečnim sistemom ("file syst
 var board = new firmata.Board("/dev/ttyACM0", function(){// ACM (Abstract Control Model)
                                                          // za serijsko komunikacijo z Arduinom (lahko je USB)
     console.log("Priklop na Arduino");
-    board.pinMode(13, board.MODES.OUTPUT); // Posamezni pin konfiguriramo, da deluje kot vhod ali izhod ,dela tudi brez konfiguracije
+    board.pinMode(13, board.MODES.OUTPUT); // Posamezni pin konfiguriramo, da deluje kot vhod ali izhod, deluje tudi brez konfiguracije
 });
 
 function handler(req, res) { // "handler", ki je uporabljen pri require("http").createServer(handler)
-    fs.readFile(__dirname + "/primer05.html", // povemo, da bomo ob zahtevi ("request") posredovali
-    function (err, data) {                    // klientu datoteko primer04.html iz diska strežnika
+    fs.readFile(__dirname + "/primer06.html", // povemo, da bomo ob zahtevi ("request") posredovali
+    function (err, data) {                    // klientu datoteko primer06.html iz diska strežnika
         if (err) {
             res.writeHead(500, {"Content-Type": "text/plain"});
             return res.end("Napaka pri nalaganju html strani.");
@@ -25,6 +25,23 @@ http.listen(8080); // strežnik bo poslušal na vratih 8080
 
 io.sockets.on("connection", function(socket) {
     socket.on("ukazArduinu", function(štUkaza) {
+        if (štUkaza == "1") {
+            board.digitalWrite(13, board.HIGH); // zapišemo +5V na p. 13
+        }
+        if (štUkaza == "0") {
+            board.digitalWrite(13, board.LOW); // zapišemo 0V na pin13
+        }
+        if (štUkaza == "2") {
+            board.digitalWrite(8, board.HIGH); // zapišemo +5V na pin8
+        }
+        if (štUkaza == "3") {
+            board.digitalWrite(8, board.LOW); // zapišemo 0V na pin8
+        }
+    });
+});
+//sosed
+io.sockets.on("connection", function(socket2) {
+    socket2.on("ukazArduinu", function(štUkaza) {
         if (štUkaza == "1") {
             board.digitalWrite(13, board.HIGH); // zapišemo +5V na p. 13
         }
